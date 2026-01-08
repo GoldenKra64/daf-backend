1e0111b0a755e526c8c6e7b6161a61c833343d53
Francisco Miguez <franmiguez64@gmail.com>
2026-01-08 15:22:24 -0500

    cors en .env

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/pos.auth.routes');
 const materiaPrimaRoutes = require('./routes/pos.materiaprima.routes');
 
 const app = express();
 
+let corsConfiguration = {
+  origin: process.env.FRONTEND_IP,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+app.use(cors(corsConfiguration));
 app.use(express.json());
 
 // RUTAS POS
@@ -11,7 +18,7 @@ app.use('/api/pos', authRoutes);
 app.use('/api/pos/materiaprima', materiaPrimaRoutes);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Endpoint not found' });
+  res.status(404).json({ message: 'Not Found' });
 });
 
 module.exports = app;
