b7eebddde67a4eb0195a501db44327e0c1fd3f1c
LordVader65 <luis.af.alobuela@gmail.com>
2026-01-18 01:09:48 -0500

    Implementacion modulo ordenes de compra

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const appAuthRoutes = require('./routes/ecom.auth.routes');
 const unidadMedidaRoutes = require('./routes/pos.unidadmedida.routes');
 const transaccionRoutes = require('./routes/pos.transaccion.routes');
 const ciudadRoutes = require('./routes/ecom.ciudad.routes');
+const ordenCompraRoutes = require('./routes/pos.ordencompra.routes');
+
 
 const app = express();
 
@@ -41,6 +43,7 @@ app.use('/api/pos/cliente', clienteRoutes);
 app.use('/api/pos/materiaprima', materiaPrimaRoutes);
 app.use('/api/pos/estandar', estandarRoutes);
 app.use('/api/pos/kardexmp', kardexMPRoutes);
+app.use('/api/pos/ordencompra', ordenCompraRoutes);
 
 // Rutas e-com
 app.use('/api/ecom/auth', appAuthRoutes);
@@ -50,6 +53,7 @@ app.use('/api/pos/unidadmedida', unidadMedidaRoutes);
 app.use('/api/pos/transaccion', transaccionRoutes);
 app.use('/api/ecom/ciudad', ciudadRoutes);
 app.use('/api/pos/categoria', categoriaRoutes);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not Found' });
